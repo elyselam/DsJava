f5949e037491745857f5d534438bbae56e8951d8
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-07-05 11:31:14 -0700

    update linkedlist

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -76,6 +76,21 @@ class LinkedList {
     public String toString() {
         return "LinkedList{" + "head=" + head + '}';
     }
+
+   public boolean hasCycle(Node head) {
+        if (head == null) return false;
+        Node fast = head.next;
+        Node slow = head;
+        //fast.next != null to make sure fast.next.next wont give nullPointerException
+        while (fast != null && fast.next != null && slow != null) {
+            if (fast == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+   }
 }
 
 
