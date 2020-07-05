fa969b9923874e404c398e43ed42ea76e106f9b2
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-07-05 12:06:58 -0700

    add tree

---
+++ src/Tree/BinarySearchTree.java
@@ -0,0 +1,33 @@
+package Tree;
+
+//all nodes on left is smaller or equal to the right
+//given tree has no dupes, so no equal
+//linear time
+//space: if balanced tree: O(log(n))
+public class BinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+    class Node {
+        int data;
+        Node left;
+        Node right;
+
+        boolean checkBST(Node root, int min, int max) {
+            if (root == null) {
+                return true;
+            }
+            if (root.data < min || root.data > max) {
+                return false;
+            }
+            //left can smallest or one value smaller than the root
+            return checkBST(root.left, min, root.data - 1) && checkBST(root.right, root.data + 1, max);
+
+        }
+        boolean checkBST(Node root) {
+            return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+    }
+
+}
+
